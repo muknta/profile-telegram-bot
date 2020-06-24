20a9e71cddc464475954aee9029c7d0337034597
heknt <hekntsnv@gmail.com>
2020-06-24 19:41:07 +0300

    age restriction; typing notation

--- profile_bot.py
+++ profile_bot.py
@@ -1,5 +1,6 @@
 import os
 from telebot import TeleBot, types
+from telebot.types import Message
 from flask import Flask, request
 from config import API_TOKEN, HEROKU_URL
 from db_helper import db
@@ -13,7 +14,7 @@ sentences = {
 	'menu': 'Menu',
 	'settings': 'Settings',
 	'get_name': 'Enter your name (2-20 characters):',
-	'get_age': 'Enter your age:',
+	'get_age': 'Enter your age (1-200):',
 	'get_gender': 'Choose your gender:'
 }
 MIN_NAME_LEN = 2
@@ -55,29 +56,35 @@ def check_any_command(cmd):
 	return commands.get(cmd)
 
 
-def is_natural(n):
+def is_natural_age(n: str):
     try:
         float(n)
     except ValueError:
         return False
     else:
-        return float(n).is_integer() and float(n) > 0
+        return float(n).is_integer() \
+        	and float(n) > 0 and float(n) < 200
+
+
+def process_exception(message: Message, error: str):
+	bot.reply_to(message, f'Exception: {error}')
+	start(message)
 
 
 @bot.message_handler(commands=['start'])
-def start(message):
+def start(message: Message):
 	markup = types.ReplyKeyboardRemove(selective=False)
 	msg = bot.send_message(message.chat.id, sentences['get_name'], reply_markup=markup)
 	bot.register_next_step_handler(msg, process_name_step)
 
 
 @bot.message_handler(commands=['help'])
-def help(message):
+def help(message: Message):
 	bot.reply_to(message, sentences['help'])
 
 
 @bot.message_handler(commands=['menu'])
-def menu(message):
+def menu(message: Message):
 	try:
 		chat_id = message.chat.id
 
@@ -89,10 +96,10 @@ def menu(message):
 		msg = bot.send_message(chat_id, sentences['menu'], reply_markup=markup)
 		bot.register_next_step_handler(msg, process_menu)
 	except Exception as e:
-		bot.reply_to(message, f'Exception: {e}')
+		process_exception(message, e)
 
 
-def process_menu(message):
+def process_menu(message: Message):
 	try:
 		chat_id = message.chat.id
 		param = message.text
@@ -107,11 +114,11 @@ def process_menu(message):
 			else:
 				menu(message)
 	except Exception as e:
-		bot.reply_to(message, f'Exception: {e}')
+		process_exception(message, e)
 
 
 @bot.message_handler(commands=['info'])
-def get_info(message):
+def get_info(message: Message):
 	try:
 		chat_id = message.chat.id
 		# user = users[chat_id]
@@ -128,11 +135,11 @@ def get_info(message):
 		bot.send_message(chat_id, user_data, reply_markup=markup)
 		menu(message)
 	except Exception as e:
-		bot.reply_to(message, f'Exception: {e}')
+		process_exception(message, e)
 
 
 @bot.message_handler(commands=['settings'])
-def change_info(message):
+def change_info(message: Message):
 	try:
 		chat_id = message.chat.id
 
@@ -146,11 +153,11 @@ def change_info(message):
 		msg = bot.send_message(chat_id, sentences['settings'], reply_markup=markup)
 		bot.register_next_step_handler(msg, process_info)
 	except Exception as e:
-		bot.reply_to(message, f'Exception: {e}')
+		process_exception(message, e)
 
 
 @bot.message_handler(commands=['change_name', 'change_age', 'change_gender'])
-def process_info(message):
+def process_info(message: Message):
 	try:
 		chat_id = message.chat.id
 		param = message.text
@@ -176,10 +183,10 @@ def process_info(message):
 			else:
 				change_info(message)
 	except Exception as e:
-		bot.reply_to(message, f'Exception: {e}')
+		process_exception(message, e)
 
 
-def process_name(message):
+def process_name(message: Message):
 	try:
 		chat_id = message.chat.id
 		name = message.text
@@ -204,10 +211,10 @@ def process_name(message):
 			msg = bot.reply_to(message, sentences['get_name'])
 			bot.register_next_step_handler(msg, process_name)
 	except Exception as e:
-		bot.reply_to(message, f'Exception: {e}')
+		process_exception(message, e)
 
 
-def process_name_step(message):
+def process_name_step(message: Message):
 	try:
 		chat_id = message.chat.id
 		name = message.text
@@ -231,14 +238,14 @@ def process_name_step(message):
 			msg = bot.reply_to(message, sentences['get_name'])
 			bot.register_next_step_handler(msg, process_name_step)
 	except Exception as e:
-		bot.reply_to(message, f'Exception: {e}')
+		process_exception(message, e)
 
 
-def process_age(message):
+def process_age(message: Message):
 	try:
 		chat_id = message.chat.id
 		age = message.text
-		if is_natural(age):
+		if is_natural_age(age):
 			age = int(age)
 			# user = users[chat_id]
 			# user.age = age
@@ -251,14 +258,14 @@ def process_age(message):
 			msg = bot.reply_to(message, sentences['get_age'])
 			bot.register_next_step_handler(msg, process_age)
 	except Exception as e:
-		bot.reply_to(message, f'Exception: {e}')
+		process_exception(message, e)
 
 
-def process_age_step(message):
+def process_age_step(message: Message):
 	try:
 		chat_id = message.chat.id
 		age = message.text
-		if is_natural(age):
+		if is_natural_age(age):
 			age = int(age)
 			# user = users[chat_id]
 			# user.age = age
@@ -269,10 +276,10 @@ def process_age_step(message):
 			msg = bot.reply_to(message, sentences['get_age'])
 			bot.register_next_step_handler(msg, process_age_step)
 	except Exception as e:
-		bot.reply_to(message, f'Exception: {e}')
+		process_exception(message, e)
 
 
-def set_gender(message):
+def set_gender(message: Message):
 	try:
 		chat_id = message.chat.id
 
@@ -284,11 +291,12 @@ def set_gender(message):
 		msg = bot.send_message(chat_id, sentences['get_gender'], reply_markup=markup)
 		bot.register_next_step_handler(msg, process_gender)
 	except Exception as e:
-		bot.reply_to(message, f'Exception: {e}')
+		process_exception(message, e)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def iq_callback(query):
+	print(query, type(query))
 	data = query.data
 	if data in ['female', 'male']:
 		bot.clear_step_handler_by_chat_id(chat_id=query.message.chat.id)
@@ -300,7 +308,7 @@ def get_gender_callback(query):
 	send_gender_result(query.message, query.data)
 
 
-def send_gender_result(message, gender):
+def send_gender_result(message: Message, gender:str):
 	try:
 		chat_id = message.chat.id
 		bot.send_chat_action(chat_id, 'typing')
@@ -311,10 +319,10 @@ def send_gender_result(message, gender):
 		db.update_row(chat_id, 'gender', gender)
 		menu(message)
 	except Exception as e:
-		bot.reply_to(message, f'Exception: {e}')
+		process_exception(message, e)
 
 
-def process_gender(message):
+def process_gender(message: Message):
 	try:
 		chat_id = message.chat.id
 		gender = message.text
@@ -329,11 +337,11 @@ def process_gender(message):
 		else:
 			set_gender(message)
 	except Exception as e:
-		bot.reply_to(message, f'Exception: {e}')
+		process_exception(message, e)
 
 
 @bot.message_handler(commands=['delete'])
-def delete_info(message):
+def delete_info(message: Message):
 	try:
 		chat_id = message.chat.id
 
@@ -346,7 +354,7 @@ def delete_info(message):
 			db.delete_row(chat_id)
 			start(message)
 	except Exception as e:
-		bot.reply_to(message, f'Exception: {e}')
+		process_exception(message, e)
 
 
 # @bot.message_handler(func=lambda msg: True, content_types=['text'])
@@ -358,7 +366,6 @@ def delete_info(message):
 # bot.load_next_step_handlers()
 
 # bot.polling()
-
 
 
 if __name__ == '__main__':
